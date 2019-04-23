4adbae2ba03798cbb7b2b17c4f2969eed0b8ad5a
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2019-04-23 18:24:35 -0400

    Add count num files

---
+++ count-things/count-num-files.sh
@@ -0,0 +1,5 @@
+# use wc count of the number of lines (-l) in ls -1
+# count the number of files in the current directory:
+ls -1 | wc -l
+# count the number of files in another directory:
+ls -1 .. | wc -l
